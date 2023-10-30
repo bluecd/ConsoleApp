cfbd94b782d601564d536fcf97539c48880fc310
zbigniew solarewicz <zsolarewicz@gmail.com>
2023-10-30 18:40:28 +0100

    dzien 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program do rankowania pracownikow
+
+User user1 = new User();
+User user2 = new User();
+User user3 = new User();
+User user4 = new User();
+
+user1.login = "Adam"   ;
+
+
+class User
+{
+    public string login;
+    public string password;
+}
+
