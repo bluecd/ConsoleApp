b4d8772f92003619908dc9efffe4474f2c2e1b8e
zbigniew solarewicz <zsolarewicz@gmail.com>
2023-10-30 18:40:28 +0100

    dzien 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// zadanie domowe: Kurs C# Od Podstaw: Podstawy Dzień 4: Zmienne i podstawowe operacje
+
+char imieOsoby = "Iwona";
+char plecOsoby = "kobieta";
+int wiekOsoby = "53";
+
+if (plecOsoby == "kobieta") && (imieOsoby == "Iwona") && (wiekOsoby > 50);
+        {
+    Console.WriteLine("Ta osoba to " ImieOsoby ", jest" plecOsoby " i ma " wiekOsoby);
+        }
