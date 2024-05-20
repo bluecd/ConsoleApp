07d5a2ce4e9fec8534581a97b211be80bb365e29
zbigniew solarewicz <zsolarewicz@gmail.com>
2024-05-20 22:08:35 +0200

    day 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.ComponentModel.Design;
+
+var myName = "Zbigg";
+var mySex = "mezczyzna";
+var myAge = 54;
+
+if (mySex == "kobieta" && myAge < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (myName == "Ewa" && myAge == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (mySex == "mezczyzna" && myAge < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
